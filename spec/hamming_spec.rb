b632fb374dfd90b0982ed3716306056911be0f63
Chase Dunagan <cdunagan@utexas.edu>
2017-08-02 21:28:51 -0500

    add hamming test for 1 different strand

--- spec/hamming_spec.rb
+++ spec/hamming_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "hamming test" do
   it "tests identical strands" do
     expect(Hamming.compute('A', 'A')).to eq(0)
   end
+
+  it "tests one different strand" do
+    expect(Hamming.compute('A', 'B')).to eq(1)
+  end
 end
