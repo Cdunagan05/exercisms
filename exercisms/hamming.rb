16b1d591cb2004c62f7c1798573e4cc4d0850c8e
Chase Dunagan <cdunagan@utexas.edu>
2017-08-02 21:52:59 -0500

    refactor hamming file to take out length test from method

--- exercisms/hamming.rb
+++ exercisms/hamming.rb
@@ -1,16 +1,18 @@
 class Hamming
   def self.compute(a, b)
-    if a.length != b.length
-      raise ArgumentError.new
-    end
+    length_test(a, b)
     total = 0
     a.chars.each_with_index do |i, k|
       if b[k] != i
         total += 1
-      else
-        total
       end
     end
     total
+  end
+
+  def self.length_test(a, b)
+    if a.length != b.length
+      raise ArgumentError.new
+    end
   end
 end
