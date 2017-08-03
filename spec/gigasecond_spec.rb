2a6c75cba01235df05db559163c19cc6d987e28e
Chase Dunagan <cdunagan@utexas.edu>
2017-08-03 18:05:52 -0500

    add test for full time

--- spec/gigasecond_spec.rb
+++ spec/gigasecond_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "gigasecond test" do
   it "tests date only specification of time" do
     expect(Time.utc(2043, 1, 1, 1, 46, 40)).to eq(Gigasecond.from(Time.utc(2011, 4, 25, 0, 0, 0)))
   end
+
+  it "tests full time specification" do
+    expect(Time.utc(2046, 10, 2, 23, 46, 40)).to eq(Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0)))
+  end
 end
