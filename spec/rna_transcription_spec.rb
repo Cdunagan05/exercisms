18c249bd5fd0fb3f787b95ee850088cdf97c8b4d
Chase Dunagan <cdunagan@utexas.edu>
2017-08-07 18:53:45 -0500

    add test for 2 parts of dna strand

--- spec/rna_transcription_spec.rb
+++ spec/rna_transcription_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "RNA test" do
   it "tests that it can convert one part of strand" do
     expect(Complement.of_dna('C')).to eq('G')
   end
+
+  it "tests that it can convert two parts of a strand" do
+    expect(Complement.of_dna('CA')).to eq('GU')
+  end
 end
