d2f267b6fbd19075766b0918cef361196cc7708b
Chase Dunagan <cdunagan@utexas.edu>
2017-08-08 22:08:35 -0500

    add change to method to make test for sound of 7 is plong test pass

--- exercisms/raindrops.rb
+++ exercisms/raindrops.rb
@@ -1,11 +1,26 @@
 class Raindrops
   def self.convert(number)
-    if number == 3
-      return "Pling"
-    elsif number == 5
-      return "Plang"
+    factors = []
+    (1..number).each_with_index do |i|
+      if number % i == 0
+        factors << i
+      end
+    end
+    if factors.include?(3)
+      return 'Pling'
+    elsif factors.include?(5)
+      return 'Plang'
+    elsif factors.include?(7)
+      return 'Plong'
     else
       return number.to_s
     end
+    # if number == 3
+    #   return "Pling"
+    # elsif number == 5
+    #   return "Plang"
+    # else
+    #   return number.to_s
+    # end
   end
 end
