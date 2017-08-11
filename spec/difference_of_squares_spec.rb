7af0b1f3ca442cd8c2ad3aeba160b3d2a63c02b3
Chase Dunagan <cdunagan@utexas.edu>
2017-08-10 21:00:03 -0500

    add test for square of sum of 5 and passes

--- spec/difference_of_squares_spec.rb
+++ spec/difference_of_squares_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "difference of squares tests" do
   it "tests the square of the sum of 1" do
     expect(Squares.new(1).square_of_sum).to eq(1)
   end
+
+  it "tests the square of the sum of 5" do
+    expect(Squares.new(5).square_of_sum).to eq(225)
+  end
 end
