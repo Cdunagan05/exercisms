66ef59303cad19eafc35c14d0c648d3d47cef93a
Chase Dunagan <cdunagan@utexas.edu>
2017-08-11 21:11:23 -0500

    add test for a real pangram being true, and test passes

--- spec/pangram_spec.rb
+++ spec/pangram_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe "Pangram tests" do
     expect(Pangram.pangram?('')).to eq(false)
   end
 
+  it "tests a pangram in lower case" do
+    phrase = "the quick brown fox jumps over the lazy dog"
+    expect(Pangram.pangram?(phrase)).to eq(true)
+  end
+
 
 end
