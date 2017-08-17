938a7e3b46a9c916d230f2af990b5b56dc254061
Chase Dunagan <cdunagan@utexas.edu>
2017-08-16 21:06:00 -0500

    add test for finding all primes 2 and under

--- spec/sieve_spec.rb
+++ spec/sieve_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "Sieve tests" do
     expected = []
     expect(Sieve.new(1).primes).to eq(expected)
   end
+
+  it "finds the first prime number" do
+    expected = [2]
+    expect(Sieve.new(2).primes).to eq(expected)
+  end
 end
