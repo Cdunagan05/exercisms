08b89e2fe0d7dd236073b1dc248c42cbb93c710b
Chase Dunagan <cdunagan@utexas.edu>
2017-08-17 18:33:13 -0500

    add code to to_roman method to get test for double digit roman numeral conversion to pass

--- exercisms/roman_numerals.rb
+++ exercisms/roman_numerals.rb
@@ -1,8 +1,17 @@
+require 'pry'
+
 class Integer
   def to_roman
-    romans = {1 => "I", 2 => "II", 3 => "III", 4 => "IV",
+    roman_number = []
+    romans_ones = {1 => "I", 2 => "II", 3 => "III", 4 => "IV",
               5 => "V", 6 => "VI", 7 => "VII", 8 => "VIII",
               9 => "IX"}
-    romans[self]
+    romans_tens = {1 => "X", 2 => "XX", 3 => "XXX", 4 => "XL",
+              5 => "L", 6 => "LX", 7 => "LXX", 8 => "LXXX",
+              9 => "XC"}
+    num = self.to_s.chars
+    roman_number << romans_tens[num[-2].to_i]
+    roman_number << romans_ones[num[-1].to_i]
+    roman_number.join("")
   end
 end
